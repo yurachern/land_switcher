121fd8217c397ed5727c760f627fc8e3d35cf56f
yurachern <tchernykh.yurij@ukr.net>
2024-07-06 18:31:51 +0300

    jwt_ls_update_migration_20240706170855

--- plugins/jtl_land_switcher/Migrations/Migration20240706170855.php
+++ plugins/jtl_land_switcher/Migrations/Migration20240706170855.php
@@ -20,7 +20,16 @@ class Migration20240706170855 extends Migration implements IMigration
      */
     public function up()
     {
-        $this->execute("");
+        $this->execute("
+        CREATE TABLE IF NOT EXISTS `jtl_land_switcher_urls` (
+              `id` INT NOT NULL AUTO_INCREMENT,
+              `url` VARCHAR(255) NOT NULL,
+              `tland_cISO` VARCHAR(5) NOT NULL,
+              PRIMARY KEY (`id`),
+            INDEX tland_cISO_index (tland_cISO),
+            FOREIGN KEY (tland_cISO) REFERENCES tland(cISO) ON DELETE CASCADE
+              ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+        ");
     }
 
     /**
