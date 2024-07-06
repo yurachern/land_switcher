19974798aea60d2d87ab17e5ce8966a465c6680f
yurachern <tchernykh.yurij@ukr.net>
2024-07-07 00:18:53 +0300

    jwt_ls_add_model_backend_controller

---
+++ plugins/jtl_land_switcher/ModelBackendController.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Plugin\jtl_land_switcher;
+
+use JTL\Helpers\Request;
+use JTL\Plugin\PluginInterface;
+use JTL\Router\Controller\Backend\GenericModelController;
+use JTL\Shop;
+use JTL\Smarty\JTLSmarty;
+use Plugin\jtl_land_switcher\Models\ModelSwitcher;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Class ModelBackendController
+ * @package Plugin\jtl_test
+ */
+class ModelBackendController extends GenericModelController
+{
+    /**
+     * @var int
+     */
+    public int $menuID = 0;
+
+    /**
+     * @var PluginInterface
+     */
+    public PluginInterface $plugin;
+
+    /**
+     * @inheritdoc
+     */
+    public function getResponse(ServerRequestInterface $request, array $args, JTLSmarty $smarty): ResponseInterface
+    {
+        $this->smarty = $smarty;
+        $this->route = \str_replace(Shop::getAdminURL(), '', $this->plugin->getPaths()->getBackendURL());
+        $this->smarty->assign('route', $this->route);
+        $this->modelClass = ModelSwitcher::class;
+        $this->adminBaseFile = \ltrim($this->route, '/');
+        $tab = Request::getVar('action', 'overview');
+        $db = $this->getDB();
+        if ($tab != 'overview') {
+            $smarty->assign('item', ModelSwitcher::loadByAttributes(['id' => Request::getInt('id')], $db))
+                ->assign('defaultTabbertab', $this->menuID);
+        }
+        $smarty->assign('step', $tab)
+            ->assign('tab', $tab)
+            ->assign('action', $this->plugin->getPaths()->getBackendURL());
+
+        $response = $this->handle(__DIR__ . "/adminmenu/templates/jtl_land_switcher_list_tab.tpl");
+        if ($this->step === 'detail') {
+            $smarty->assign('defaultTabbertab', $this->menuID);
+        }
+        return $response;
+    }
+}
